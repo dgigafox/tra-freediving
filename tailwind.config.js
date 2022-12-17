bfad22646e32be8ea67df949726210b367e2814d
Darren Gegantino <darrengegantino@gmail.com>
2022-12-17 23:03:47 +0800

    Setup tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js, jsx}",
+    "./public/**/*.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
